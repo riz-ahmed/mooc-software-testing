8a59fbfda7420c05d0dac93c6d85f9db3c9ee60e
riz-ahmed <rzwn.hmd@gmail.com>
2021-01-16 13:31:38 +0100

    test runs with leap year module

--- src/main/java/tudelft/roman/RomanNumeralMain.java
+++ src/main/java/tudelft/roman/RomanNumeralMain.java
@@ -1,13 +1,20 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralMain {
 
+    private RomanNumeral roman;                             // a global var within this class with private access
+    @BeforeEach                                             // this is like using fixtures in pytest
+    public void intialise(){
+        this.roman = new RomanNumeral();
+    }
+
     @Test
     void singleNUmber(){
-        RomanNumeral roman = new RomanNumeral();            // create RomanNumeral class instance
+
         int result =  roman.convert("I");               // invoke roman class methods and store result in new variable
 
         Assertions.assertEquals(1, result);
@@ -15,7 +22,7 @@ public class RomanNumeralMain {
 
     @Test
     void testWithMoreNumbers(){
-        RomanNumeral roman = new RomanNumeral();
+
         int result = roman.convert("VIII");
 
         Assertions.assertEquals(8, result);
@@ -23,7 +30,7 @@ public class RomanNumeralMain {
 
     @Test
     void testWithSubNumbers(){
-        RomanNumeral roman = new RomanNumeral();
+
         int result = roman.convert("IV");
 
         Assertions.assertEquals(4, result);
